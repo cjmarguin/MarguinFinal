1e9aa5f2c2205a0da2de0ce9e5da84799fece85c
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-12-03 23:21:22 +0000

    bread

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+var head = '';
+var img = '';
+var desc = '';
+
 //add your app get here for the home page '/'
+
+app.get('/', function(req, res){
+    fetchInfo(true);
+
+    res.render('index', {header:head, image:img , description:desc});
+}
 //within the get call your function and render the ejs page
+function fetchInfo(yee){
+    var SpaceAPI = 'https://csuserversidewebdevfinal.herokuapp.com/';
+    fetch(yee? SpaceAPI:SpaceAPI)
+    .then(resp => resp.json())
+    .then(data =>{
+        header = data.head;
+        image = data.img;
+        description = data.desc;
+
+        
+    
+    });
+
+}
+
 app.listen(port, function () {
 });
